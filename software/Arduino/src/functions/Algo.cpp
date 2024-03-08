5f59974268607c7eafbcdc55f85a5f8dbc1a108a
notpunchnox <punchnoxpaypal@gmail.com>
2024-03-08 12:31:11 +0100

    Correction algorithme C++

--- software/Arduino/src/functions/Algo.cpp
+++ software/Arduino/src/functions/Algo.cpp
@@ -10,22 +10,20 @@ LegAngles Algo(float x, float z, float y, int duree) {
     x = taille de la patte; z = hauteur; y = rotation (cm);
     duree = durée du mouvement en millisecondes;
     */
+   //Calcul des valeurs manquantes ( Taille de la patte vu du ciel, et la taille de l'hypotenuse formé par le bout du coxa et çje bout de la patte )
     float TPatte = Racine(y, x);
-    Serial.println("\nTpatte: ");
-    Serial.print(TPatte);
-    float hyp = Racine(x, TPatte-coxa);
-    Serial.println("\nHypotenuse: ");
-    Serial.println(hyp);
+    float hyp = Racine(z, TPatte-coxa);
+
     float A1 = radiansToDegrees(atan((TPatte-coxa)/z));
     float A2 = AlKashi(femur, hyp, tibia);
-
     angles.AngleFemur = A1 + A2;
-    Serial.println(angles.AngleFemur);
+
     float A3 = AlKashi(tibia, femur, hyp);
+   
     Serial.println("\nTibia: ");
     Serial.println(A3);
 
-    angles.AngleCoxa = 90 + radiansToDegrees(atan(y / x));
+    angles.AngleCoxa = radiansToDegrees(atan(y / x)) + 90;
     angles.AngleTibia = A3;
 
     return angles;
