f8140db92ac48d7322410fdff9769d21e6a67c0e
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2024-05-17 18:25:10 +0200

    version stable

--- software/Arduino/src/animations/walk/walk.cpp
+++ software/Arduino/src/animations/walk/walk.cpp
@@ -4,69 +4,99 @@
 #include "../../functions/servo.h"
 #include "../sleep/sleep.h"
 
-// function pour faire aller une patte du robot dans les coordonnées désirés
-void setLeg(float x, float z, float y, int duree, int LEG[3], int address) {
-  if(LEG == LFL || LEG == LBR) {
-    y = y+11;
+struct LegPosition {
+  int servoChannels[3];
+  float x;
+  float y;
+  float z;
+  int address;
+  int duree;
+};
+
+void Move(LegPosition legs[]) {
+  LegConfig LEGS[sizeof(legs)];
+
+  for(int i = 0; i < sizeof(legs); ++i) {
+    LegPosition leg = legs[i];
+    int* LEG = leg.servoChannels;
+
+    if(LEG == LFL || LEG == LBR) {
+      leg.y += 11;
+    }
+    if(LEG == LFR || LEG == LBL) {
+      leg.y -= 11;
+    }
+
+    LegAngles res = Algo(leg.x, leg.z, leg.y, leg.duree);
+
+    float angles[3];
+
+    if (LEG == LML || LEG == LFL || LEG == LBL) { // Si c'est une patte de gauche, il faut inverser les angles tibia et fémur
+      angles[0] = res.AngleTibia;
+      angles[1] = 180 - res.AngleFemur;
+      angles[2] = res.AngleCoxa;
+    } else {
+      angles[0] = 180 - res.AngleTibia;
+      angles[1] = res.AngleFemur;
+      angles[2] = res.AngleCoxa;
+    }
+
+    LEGS[i].address = leg.address;
+    LEGS[i].coxa = angles[2];
+    LEGS[i].femur = angles[1];
+    LEGS[i].tibia = angles[0];
+
+    for (int j = 0; j < 3; ++j) {
+      LEGS[i].servoChannels[j] = leg.servoChannels[j];
+    }
+
+    LEGS[i].duree = leg.duree;
   }
-  if(LEG == LFR || LEG == LBL) {
-    //y = y-11;
-  }
-  LegAngles res = Algo(x, z, y, duree);
 
-  float angles[3];
-
-  if (LEG == LML || LEG == LFL || LEG == LBL) {//Si c'est une patte de gauche alors il faut inverser les angles tibia et femur
-    
-    angles[0] = res.AngleTibia;
-    angles[1] = 180-res.AngleFemur;
-    angles[2] = res.AngleCoxa;
-  } else {
-    angles[0] = 180-res.AngleTibia;
-    angles[1] = res.AngleFemur;
-    angles[2] = res.AngleCoxa;
-  };
-
-  setServo(LEG, 3, angles, address, duree);
+  setLegs(LEGS);
 }
 
-// R: Rayon cercle virtuel: 5cm ( extension de la patte lors de la marche ).
+// R: Rayon cercle virtuel: 5cm (extension de la patte lors de la marche).
 // Matrice de positions dans un environnement 3D (x, z, y, ms, attente) => hauteur, extension, rotation coxa en cm, temps du mouvement, temps d'attente une fois le mouvement fait.
 
 float R = 2.5;
 
 float WalkUpMatrice[][5] = {
-  {PX, PZ, PY, 20, 0},//Patte par défaut
-  {PX, 1, PY, 600, 0},//Patte levé sur l'axe Z
-  {PX, 1, 2*R, 200, 0},//Avancer la patte sur l'axe Y
-  {PX, PZ, 2*R, 600, 0},//Reposer la patte sur l'axe Z
+  {PX, PZ, PY, 20, 0},      // Patte par défaut
+  {PX, 1, PY, 600, 0},      // Patte levée sur l'axe Z
+  {PX, 1, 2*R, 200, 0},     // Avancer la patte sur l'axe Y
+  {PX, PZ, 2*R, 600, 0},    // Reposer la patte sur l'axe Z
   {PX, PZ, R, 500, 0},
   {PX, PZ, -R, 500, 0},
-
   {PX, PZ-5, -R, 500, 0},
   {PX, PZ, PY, 500, 0},
 };
+
 float WalkBottomMatrice[][5] = {
-  {PX, PZ, PY, 500, 1000},//Patte par défaut
-  {PX, PZ, 2*R, 700, 200},//Patte recule sur l'axe Y
+  {PX, PZ, PY, 500, 1000},  // Patte par défaut
+  {PX, PZ, 2*R, 700, 200},  // Patte recule sur l'axe Y
 };
 
 void Walk() {
   for (int i = 0; i < sizeof(WalkUpMatrice) / sizeof(WalkUpMatrice[0]); ++i) {
-    int ReverseMatrice = sizeof(WalkUpMatrice) - i;
-    //LEG FRONT RIGHT
-    setLeg(WalkUpMatrice[i][0], WalkUpMatrice[i][1], WalkUpMatrice[i][2], WalkUpMatrice[i][3], LFL, 1);
-    setLeg(WalkUpMatrice[i][0], WalkUpMatrice[i][1], WalkUpMatrice[i][2], WalkUpMatrice[i][3], LBR, 0);
-    //setLeg(WalkUpMatrice[i][0], WalkUpMatrice[i][1], WalkUpMatrice[i][2], WalkUpMatrice[i][3], LBL, 1);
-    
-    //LEG
-    if(i ==1 || i==2) {
+    int ReverseMatrice = sizeof(WalkUpMatrice) / sizeof(WalkUpMatrice[0]) - i;
 
-      //setLeg(WalkBottomMatrice[i-1][0], WalkBottomMatrice[i][1], WalkBottomMatrice[i-1][2], WalkBottomMatrice[i-1][3], LFR, 0);
-      //setLeg(WalkBottomMatrice[i-1][0], WalkBottomMatrice[i][1], WalkBottomMatrice[i-1][2], WalkBottomMatrice[i-1][3], LML, 1);
-      //setLeg(WalkBottomMatrice[i-1][0], WalkBottomMatrice[i-1][1], -WalkBottomMatrice[i-1][2], WalkBottomMatrice[i-1][3], LBR, 0);
+    LegPosition legs[2];
+    legs[0].address = 1;
+    legs[0].duree = WalkUpMatrice[i][3];
+    memcpy(legs[0].servoChannels, LFL, sizeof(LFL));
+    legs[0].x = WalkUpMatrice[i][0];
+    legs[0].z = WalkUpMatrice[i][1];
+    legs[0].y = WalkUpMatrice[i][2];
 
-    }
+    legs[1].address = 0;
+    legs[1].duree = WalkUpMatrice[i][3];
+    memcpy(legs[1].servoChannels, LBR, sizeof(LBR));
+    legs[1].x = WalkUpMatrice[i][0];
+    legs[1].z = WalkUpMatrice[i][1];
+    legs[1].y = WalkUpMatrice[i][2];
+
+    Move(legs);
     
     delay(WalkUpMatrice[i][4]);
   }
--- software/Arduino/src/functions/Servo.cpp
+++ software/Arduino/src/functions/Servo.cpp
@@ -68,53 +68,44 @@ void moveServosSmoothly(int servoChannels[], int numChannels, int duration, floa
   Serial.println(startAngles[leg].tibia);
 }
 
-void setLegs(int servoChannels[], int numChannels, float angles[], int address, int duree) {
-  if(startAngles[servoChannels[0]].femur > 0) {
-      unsigned long startTime = millis(); // Heure de début
-      int leg = servoChannels[0];
-      float legStartAngles[numChannels] = {startAngles[leg].coxa, startAngles[leg].femur, startAngles[leg].tibia};
-      float deltaAngles[numChannels];
-      float newAngles[numChannels];
 
-      for (int i = 0; i < numChannels; ++i) {
-          deltaAngles[i] = targetAngles[i] - legStartAngles[i];
-      }
 
-      while (millis() - startTime < duration) { // Tant que le temps écoulé est inférieur à la durée
+void setLegs(LegConfig legs[]) {
+  
+  // if(legs[0].femur > 0) {
+    
+  // }
+  unsigned long startTime = millis(); // Heure de début
+  for(int ii = 0; ii < sizeof(legs); ++ii) {
+    
+    int legAdress = legs[ii].servoChannels[0];//definir l'adresse de la patte
+    LegConfig leg = legs[ii];//definir la patte
+
+    float legStartAngles[3] = {startAngles[legAdress].coxa, startAngles[legAdress].femur, startAngles[legAdress].tibia};
+    float deltaAngles[3];//Calcul le delta entre l'angle de début et l'angle à atteindre
+    float newAngles[3];//variable pour stocker les nouveaux angles dans le mouvement progressif
+
+    //set deltaAngles
+    deltaAngles[0] = leg.coxa - legStartAngles[0];
+    deltaAngles[1] = leg.femur - legStartAngles[1];
+    deltaAngles[2] = leg.tibia - legStartAngles[2];
+
+    while (millis() - startTime < leg.duree) { // Tant que le temps écoulé est inférieur à la durée
           unsigned long currentTime = millis();
-          float progress = (float)(currentTime - startTime) / duration;
+          float progress = (float)(currentTime - startTime) / leg.duree;
 
-          for (int i = 0; i < numChannels; ++i) {
+          for (int i = 0; i < 3; ++i) {
               newAngles[i] = legStartAngles[i] + deltaAngles[i] * progress;
           }
 
-          for (int i = 0; i < numChannels; ++i) {
-              if (address == 1) {
-                  pwm2.writeMicroseconds(servoChannels[i], pulseWidth(newAngles[i]));
+          for (int i = 0; i < 3; ++i) {
+              if (leg.address == 1) {
+                  pwm2.writeMicroseconds(legs[ii].servoChannels[i], pulseWidth(newAngles[i]));
               } else {
-                  pwm.writeMicroseconds(servoChannels[i], pulseWidth(newAngles[i]));
+                  pwm.writeMicroseconds(legs[ii].servoChannels[i], pulseWidth(newAngles[i]));
               }
           }
       }
-
-      for (int i = 0; i < numChannels; ++i) {
-          startAngles[leg].coxa = newAngles[0];
-          startAngles[leg].femur = newAngles[1];
-          startAngles[leg].tibia = newAngles[2];
-      }
-  } else {
-    for (int i = 0; i < numChannels; ++i) {
-        if (address == 1) {
-          pwm2.writeMicroseconds(servoChannels[i], pulseWidth(angles[i]));
-        } else {
-          pwm.writeMicroseconds(servoChannels[i], pulseWidth(angles[i]));
-        }
-    }
-    for (int i = 0; i < numChannels; ++i) {
-      startAngles[servoChannels[0]].coxa = angles[0];
-      startAngles[servoChannels[0]].femur = angles[1];
-      startAngles[servoChannels[0]].tibia = angles[2];
-    }
   }
 }
 
