684948cb94556d2a3490cbc6144c5be700e3c481
notpunchnox <punchnoxpaypal@gmail.com>
2024-07-08 07:23:49 +0200

    Change serial controller and turn animation

--- software/Arduino/src/animations/InverseKinematic/turn/turn.cpp
+++ software/Arduino/src/animations/InverseKinematic/turn/turn.cpp
@@ -8,7 +8,7 @@ void Turn(String sens, float speed, int cycles) {
     float R = 3; // Rayon de la marche
     float SPEED = 200 * speed;
     int numLegs = 6;
-    int numSteps = 5; // Nombre de pas par séquence
+    int numSteps = 4; // Nombre de pas par séquence
 
     // Déterminer le sens de la rotation
     int direction = (sens == "left") ? 1 : -1;
@@ -16,13 +16,21 @@ void Turn(String sens, float speed, int cycles) {
     // Matrices de positions pour chaque patte
     float TurnMatrice[numLegs][numSteps][3] = {
         // {x, z, y} pour chaque patte {LFL, LML, LBL, LFR, LMR, LBR}
-        {{PX, PZ, PY}, {PX, PZ, PY}, {PX, PZ-5, PY}, {PX-direction * R, PZ-5, PY}, {PX-direction * R, PZ, PY}}, // LFL
-        {{PX, PZ -5, PY + direction * R}, {PX, PZ, PY+direction * R}, {PX, PZ, PY+direction * R}, {PX, PZ, PY-direction * R}, {PX, PZ, PY-direction * R}}, // LML
-        {{PX, PZ, PY}, {PX, PZ, PY}, {PX, PZ-5, PY}, {PX, PZ-5, PY-direction * R}, {PX, PZ, PY-direction * R}}, // LBL
+        {{PX, PZ, PY}, {PX, PZ-3, PY-direction * R}, {PX, PZ, PY-direction * R*2}, {PX, PZ, PY}}, // LFL
+        {{PX, PZ, PY}, {PX, PZ, PY - direction *R*2}, {PX, PZ-3, PY}, {PX, PZ-3, PY}},
+        {{PX, PZ, PY}, {PX-direction * R, PZ-3, PY}, {PX-direction * R*2, PZ, PY}, {PX, PZ, PY}},
+
+        {{PX, PZ, PY}, {PX, PZ, PY+direction*R*2}, {PX, PZ-3, PY}, {PX, PZ-3, PY}},
+        {{PX, PZ, PY}, {PX, PZ-3, PY+direction*R}, {PX, PZ, PY+direction*R*2}, {PX, PZ, PY}},
+        {{PX, PZ, PY}, {PX-direction*R*2, PZ, PY}, {PX, PZ-3, PY}, {PX, PZ-3, PY}},
+
+    //     {{PX, PZ, PY}, {PX, PZ, PY}, {PX, PZ-5, PY}, {PX-direction * R, PZ-5, PY}, {PX-direction * R, PZ, PY}}, // LFL
+    //     {{PX, PZ -5, PY + direction * R}, {PX, PZ, PY+direction * R}, {PX, PZ, PY+direction * R}, {PX, PZ, PY-direction * R}, {PX, PZ, PY-direction * R}}, // LML
+    //     {{PX, PZ, PY}, {PX, PZ, PY}, {PX, PZ-5, PY}, {PX, PZ-5, PY-direction * R}, {PX, PZ, PY-direction * R}}, // LBL
         
-        {{PX - direction * R, PZ-5, PY}, {PX-direction * R, PZ, PY}, {PX-direction * R, PZ, PY}, {PX+direction * R, PZ, PY}, {PX+direction * R, PZ, PY}}, // LFR
-        {{PX, PZ, PY}, {PX, PZ, PY}, {PX, PZ-5, PY}, {PX, PZ-5, PY+direction * R}, {PX, PZ, PY+direction * R}}, // LMR
-        {{PX, PZ -5, PY - direction * R}, {PX, PZ, PY-direction * R}, {PX, PZ, PY-direction * R}, {PX, PZ, PY+direction * R}, {PX, PZ, PY+direction * R}} // LBR
+    //     {{PX - direction * R, PZ-5, PY}, {PX-direction * R, PZ, PY}, {PX-direction * R, PZ, PY}, {PX+direction * R, PZ, PY}, {PX+direction * R, PZ, PY}}, // LFR
+    //     {{PX, PZ, PY}, {PX, PZ, PY}, {PX, PZ-5, PY}, {PX, PZ-5, PY+direction * R}, {PX, PZ, PY+direction * R}}, // LMR
+    //     {{PX, PZ -5, PY - direction * R}, {PX, PZ, PY-direction * R}, {PX, PZ, PY-direction * R}, {PX, PZ, PY+direction * R}, {PX, PZ, PY+direction * R}} // LBR
     };
 
     for (int cycle = 0; cycle < cycles; ++cycle) {
